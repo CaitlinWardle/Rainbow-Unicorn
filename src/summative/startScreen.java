badd6d9b5f1af91e8c32baa1778f1adc7bb418bd
CaitlinWardle <71661061+CaitlinWardle@users.noreply.github.com>
2021-06-06 17:39:00 -0400

    Update startScreen.java

--- src/summative/startScreen.java
+++ src/summative/startScreen.java
@@ -119,7 +119,9 @@ BufferedImage logo;
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        this.dispose();
+        Instructions play= new Instructions();
+        play.setVisible (true);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
